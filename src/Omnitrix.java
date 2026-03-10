f3157abbbffc2b2ec286d7dcc7b6a7df96541a8d
eduardonk9999 <thiago.novak7@gmail.com>
2026-03-10 19:56:51 -0300

    start project

---
+++ src/Omnitrix.java
@@ -0,0 +1,13 @@
+public class Omnitrix {
+    // Tipos primitivos
+    int totalAliens = 10;
+    double energia = 99.5;
+    boolean ativado = true;
+    char inicial =  'B';
+
+
+    String nomeProprietario = "Ben Tennyson";
+
+    final int MAX_TEMPO_TRANSFORMACAO = 600; //segundos
+
+}
